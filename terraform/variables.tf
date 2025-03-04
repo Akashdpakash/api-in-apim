707188997ffb517f52e293e309dff6d779222d89
Akashdpakash <54543615+Akashdpakash@users.noreply.github.com>
2025-03-04 22:07:48 +0530

    updating apim

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "resource_group_name" {
   description = "The name of the resource group."
   type        = string
-  default     = "test-resource-group"
+  default     = "api-resource-group"
 }
 
 variable "location" {
