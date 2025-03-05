641434e67d31fa3f0fe8bd0713ab9b247de8c43d
Akashdpakash <54543615+Akashdpakash@users.noreply.github.com>
2025-03-05 15:22:47 +0530

    updating action file

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,36 +1,35 @@
-variable "resource_group_name" {
-  description = "The name of the resource group."
-  type        = string
-  default     = "api-resource-group"
-}
-
 variable "location" {
-  description = "The Azure region."
+  description = "Azure region to deploy resources"
   type        = string
   default     = "canadacentral"
 }
 
-variable "apim_name" {
-  description = "The name of the API Management service."
+variable "resource_group_name" {
+  description = "Name of the resource group"
   type        = string
-  default     = "hello-api-new"  # Change this to a unique name
+  default     = "api-rg"
+}
+
+variable "apim_name" {
+  description = "Name of the API Management instance"
+  type        = string
+  default     = "hello-api-new"
 }
 
 variable "publisher_name" {
-  description = "The name of the publisher."
+  description = "APIM publisher name"
   type        = string
-  default     = "Aspiringakashdp"
+  default     = "GMAIl"
 }
 
 variable "publisher_email" {
-  description = "The email of the publisher."
+  description = "APIM publisher email"
   type        = string
-  default     = "Aspiringakashdp@gmail.com"
+  default     = "akashdpakash@gmail.com"
 }
 
 variable "jwt_secret" {
-  description = "The JWT secret for token validation."
+  description = "A secret value used in the JWT policy for validation (for testing only)"
   type        = string
-  sensitive   = true
-  default     = "my-secure-jwt-secret-key-1234567890"
+  default     = "mySecretJWTKey"
 }
